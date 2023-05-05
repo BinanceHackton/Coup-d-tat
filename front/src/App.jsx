8d500351f2f960fa8dcab6804c50b9affee40062
c0c0pang <coc1789@naver.com>
2023-05-06 01:30:24 +0900

    feat: recoil를 쓰기 위한 RecoilRoot 추가

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,16 +5,19 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Navigation from './nav/Navigation';
 import Footer from './footer/Footer';
 import Content from './contents/Content';
+import { RecoilRoot } from 'recoil';
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Navigation />
-      <Routes>
-        <Route path='' element={<Content />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Navigation />
+        <Routes>
+          <Route path='' element={<Content />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
