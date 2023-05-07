52e590a8fe79df54334c4bc4333c2ea30afbf86c
c0c0pang <coc1789@naver.com>
2023-05-07 22:49:18 +0900

    revert: 백업

--- front/src/atom/atom.js
+++
@@ -1,9 +0,0 @@
-import { atom } from 'recoil'
-export const buyButtonState = atom({
-    key: 'buyButtonState',
-    default: false
-})
-export const GameButtonState = atom({
-    key: 'GameButtonState',
-    default: false
-})
