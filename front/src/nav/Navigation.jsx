d439da1852161a892034f189ca6a081f35d09136
c0c0pang <coc1789@naver.com>
2023-05-08 02:56:37 +0900

    feat: 각 네비게이션 버튼에 맞는 이벤트 추가

--- front/src/nav/Navigation.jsx
+++ front/src/nav/Navigation.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useMemo } from 'react'
 import {
     NavDiv,
     LogoText,
@@ -7,35 +7,49 @@ import {
     ImageTextDiv,
     ImageText
 } from '../styled_component/styled_nav'
-import Rank from '../img/Rank.svg'
-import Wallet from '../img/Wallet.svg'
-import Warehouse from '../img/Warehouse.svg'
-function Navigation() {
+import RankIcon from '../img/Rank.svg'
+import WalletIcon from '../img/Wallet.svg'
+import WarehouseIcon from '../img/Warehouse.svg'
+import { meta } from '../wallet/Metamask'
+import { rank } from '../rank/Rank'
+import { exchange } from '../exchange/Exchange'
+import { useRecoilState } from 'recoil'
+import { NavButtonState } from '../atom/atom'
+import ConnectPage from '../wallet/ConnectPage'
+const images = [{ img: RankIcon, text: 'Rank', handler: 'rank' }, { img: WarehouseIcon, text: 'Exchange', handler: 'exchange' }, { img: WalletIcon, text: 'Wallet', handler: 'wallet' }];
+
+function NavImage(props) {
     return (
-        <NavDiv>
-            <LogoText>coup d'État</LogoText>
-            <ImageDiv>
-                <ImageTextDiv>
-                    <Image src={Rank} />
-                    <ImageText>
-                        Rank
-                    </ImageText>
-                </ImageTextDiv>
-                <ImageTextDiv>
-                    <Image src={Warehouse} />
-                    <ImageText>
-                        Exchange
-                    </ImageText>
-                </ImageTextDiv>
-                <ImageTextDiv>
-                    <Image src={Wallet} />
-                    <ImageText>
-                        Wallet
-                    </ImageText>
-                </ImageTextDiv>
-            </ImageDiv>
-        </NavDiv>
-    )
+        <ImageTextDiv onClick={() => props.onClick(props.handler)}>
+            <Image src={props.img} alt={props.text} />
+            <ImageText>{props.text}</ImageText>
+        </ImageTextDiv>
+    );
+}
+function Navigation() {
+    const [select, setSelect] = useRecoilState(NavButtonState)
+    const handleButton = (handler) => {
+        setSelect({ ...select, [handler]: true })
+    }
+    const render = () => {
+        console.log(select)
+        if (select.wallet) {
+            return <ConnectPage />
+        }
+    }
+    return (
+        <>
+            <NavDiv>
+                <LogoText>coup d'État</LogoText>
+                <ImageDiv>
+                    {images.map((image, index) => (
+                        <NavImage key={index} img={image.img} text={image.text} onClick={handleButton} handler={image.handler} />
+                    ))}
+                </ImageDiv>
+            </NavDiv>
+            {render()}
+        </>
+    );
 }
 
 export default Navigation
