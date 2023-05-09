b8203aef85a168ded2e30fdd29c5181066c08b4d
c0c0pang <coc1789@naver.com>
2023-05-09 17:35:57 +0900

    feat: metamask wallet에 대한 정보를 출력하도록 기능 추가

--- front/src/wallet/ConnectPage.jsx
+++ front/src/wallet/ConnectPage.jsx
@@ -5,7 +5,12 @@ import {
     TitleText,
     WalletImage,
     WalletListdiv,
-    WalletText
+    WalletText,
+    WalletInfoAccount,
+    WalletInfoBalance,
+    WalletInfoDiv,
+    WalletInfoNetworkName,
+    WalletInfoTicket
 } from '../styled_component/styled_wallet'
 import MetamaskIcon from '../img/Metamask.svg'
 import CloseIcon from '../img/Close.svg'
@@ -13,8 +18,10 @@ import { CloseButton, CloseButtonDiv } from '../styled_component/styled_buy'
 import { useRecoilState } from 'recoil'
 import { NavButtonState } from '../atom/atom'
 import { meta } from './Metamask'
+import useMetamask from './useMetamask'
 function ConnectPage() {
     const [select, setSelect] = useRecoilState(NavButtonState)
+    const { account, balance, networkId, networkName } = useMetamask();
     const handleButton = (handler) => {
         setSelect({ ...select, [handler]: false })
     }
@@ -33,6 +40,12 @@ function ConnectPage() {
                     <WalletImage src={MetamaskIcon} />
                     <WalletText>Metamask</WalletText>
                 </WalletListdiv>
+                <WalletInfoDiv>
+                    <WalletInfoNetworkName>networkName: {networkName}</WalletInfoNetworkName>
+                    <WalletInfoAccount>account: {account?.slice(0, 6) + ' ... ' + account?.slice(38, 42)}</WalletInfoAccount>
+                    <WalletInfoBalance>balance: {balance}</WalletInfoBalance>
+                    <WalletInfoTicket>Ticket: {0}</WalletInfoTicket>
+                </WalletInfoDiv>
             </WalletDiv>
         </>
     )
